142eebd4449c9f217d5fbb7add82f96534aa9ef5
svetlozara-godzheva <n01754024@humber.ca>
2025-06-20 20:16:27 -0400

    Assignment 2 solved

---
+++ script.js
@@ -0,0 +1,34 @@
+const userInput = document.getElementById("username");
+
+const saveButton = document.getElementById("save-btn");
+
+const clearButton = document.getElementById("clear-btn");
+
+const displayArea = document.getElementById("display-name");
+
+saveButton.addEventListener("click", () => {
+    const inputValue = userInput.value;
+    localStorage.setItem("local-username", inputValue);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const localUsername = localStorage.getItem("local-username");
+    displayArea.innerText = greet(localUsername);
+});
+
+function greet(name) {
+    let now = new Date();
+    if (now.getHours() < 12) {
+        return `Good Morning ${name}!`;
+    }
+    if (now.getHours() < 17) {
+        return `Good Afternoon ${name}!`;
+    } else {
+        return `Good Evening ${name}!`;
+    }
+}
+
+clearButton.addEventListener("click", () => {
+    localStorage.removeItem("local-username");
+    displayArea.innerText = "";
+});
